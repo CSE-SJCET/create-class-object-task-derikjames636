f5907663bf671c9201a593640a4c386532f3cef4
derikjames636 <derikjames2028@cs.sjcetpalai.ac.in>
2025-07-21 06:36:12 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
-     // TODO: Create a class called Student with name, rollNumber, and grade
+     public class Main {
+    static class Student {
+        String name;
+        int rollNumber;
+        char grade;
 
-    // TODO: Create a method to display student details
+     public void displayDetails() {
+            System.out.println("Name: " + name);
+            System.out.println("Roll Number: " + rollNumber);
+            System.out.println("Grade: " + grade);
+        }
+    }
 public class Main {
       public static void main(String[] args) {
-        // TODO: Create a Student object and call the method to display details
+        student.name = "Derik";
+        student.rollNumber = 31;
+        student.grade = 'B';
+
+        
+        student.displayDetails()
     }
 }
